8f01329c06ca96a4755fd1355f992c4bbaf3ce75
peaky76 <robertjamespeacock@gmail.com>
2023-04-27 23:40:30 +0100

    feat: optional commission arg in kelly

--- pybet/staking/kelly.py
+++ pybet/staking/kelly.py
@@ -2,7 +2,12 @@ from decimal import Decimal
 from ..odds import Odds
 
 
-def kelly(true_odds: Odds, market_odds: Odds, bank: Decimal) -> Decimal:
+def kelly(
+    true_odds: Odds,
+    market_odds: Odds,
+    bank: Decimal,
+    percentage_commission: Decimal = Decimal("0"),
+) -> Decimal:
     """Calculates the stake that should be placed according to the Kelly Criterion
     [https://en.wikipedia.org/wiki/Kelly_criterion], i.e. edge over odds
     for any given true odds at any given market odds for any given bank size
@@ -13,6 +18,8 @@ def kelly(true_odds: Odds, market_odds: Odds, bank: Decimal) -> Decimal:
     :type market_odds: Odds
     :param bank: The bank available
     :type bank: Decimal
+    :param percentage_commission: The percentage commission applied to winnings
+    :type percentage_commission: Decimal
     :return: The stake to place according to the Kelly criterion
     :rtype: Decimal
 
@@ -23,10 +30,12 @@ def kelly(true_odds: Odds, market_odds: Odds, bank: Decimal) -> Decimal:
         >>> kelly(Odds(5), Odds(4), 100)
         Decimal('0.00')
     """
+    if not 0 <= percentage_commission <= 100:
+        raise ValueError("Commission must be between 0 and 100")
 
     p: Decimal = true_odds.to_probability()
     q: Decimal = 1 - p
-    odds: Decimal = market_odds.to_one()
+    odds: Decimal = market_odds.to_one() * Decimal(str(1 - percentage_commission / 100))
 
     edge: Decimal = (odds * p) - q
     kelly: Decimal = edge / odds
--- tests/test_staking.py
+++ tests/test_staking.py
@@ -9,3 +9,14 @@ class TestStaking(TestCase):
 
     def test_kelly_stake_when_bettor_does_not_have_edge(self):
         self.assertEqual(0, kelly(Odds(5), Odds(4), 100))
+
+    def test_kelly_stake_when_bettor_has_edge_and_commission_applied(self):
+        self.assertEqual(6.25, kelly(Odds(4), Odds(6), 100, 20))
+
+    def test_kelly_stake_raises_value_error_if_commission_is_less_than_0(self):
+        with self.assertRaises(ValueError):
+            kelly(Odds(4), Odds(5), 100, -1)
+
+    def test_kelly_stake_raises_value_error_if_commission_is_greater_than_100(self):
+        with self.assertRaises(ValueError):
+            kelly(Odds(4), Odds(5), 100, 101)
