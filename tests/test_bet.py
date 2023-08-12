10b5963bdd4dbf087eeafbd11646d51da52fbaa0
peaky76 <robertjamespeacock@gmail.com>
2023-08-13 00:18:10 +0100

    feat: Bet.void method

--- tests/test_bet.py
+++ tests/test_bet.py
@@ -17,6 +17,11 @@ class TestBet(TestCase):
         bet = Bet(2.50, Odds(2), lambda: False, lambda: True)
         self.assertEqual(bet.settle(), 0)
 
+    def test_bet_settle_returns_stake_if_bet_is_void(self):
+        bet = Bet(2.50, Odds(2), lambda: False, lambda: True)
+        bet.void()
+        self.assertEqual(bet.settle(), 2.50)
+
     def test_bet_settle_raises_error_if_bet_is_not_settled(self):
         bet = Bet(2.50, Odds(2), lambda: True, lambda: False)
         with self.assertRaises(ValueError):
@@ -33,3 +38,8 @@ class TestBet(TestCase):
     def test_bet_status_returns_lost_if_bet_settled_and_win_condition_is_false(self):
         bet = Bet(2.50, Odds(2), lambda: False, lambda: True)
         self.assertEqual(str(bet.status), "LOST")
+
+    def test_bet_void_voids_bet(self):
+        bet = Bet(2.50, Odds(2), lambda: False, lambda: True)
+        bet.void()
+        self.assertEqual(str(bet.status), "VOID")
