52f02771c275d248e1a6de9eca798b5faf967145
peaky76 <robertjamespeacock@gmail.com>
2023-08-12 21:39:01 +0100

    feat: Bet stake and odds attributes

--- tests/test_bet.py
+++ tests/test_bet.py
@@ -1,39 +1,39 @@
-from pybet import Bet
+from pybet import Bet, Odds
 from unittest import TestCase
 
 
 class TestBet(TestCase):
-    def test_bet_can_be_initialised_with_win_condition_and_end_condition_callbacks(
+    def test_bet_can_be_initialised(
         self,
     ):
-        self.assertTrue(Bet(lambda: None, lambda: None))
+        self.assertTrue(Bet(2.50, Odds(2), lambda: None, lambda: None))
 
     def test_is_settled_returns_true_if_end_condition_is_true_and_win_condition_is_true(
         self,
     ):
-        bet = Bet(lambda: True, lambda: True)
+        bet = Bet(2.50, Odds(2), lambda: True, lambda: True)
         self.assertTrue(bet.is_settled)
 
     def test_is_settled_returns_true_if_end_condition_is_true_and_win_condition_is_false(
         self,
     ):
-        bet = Bet(lambda: False, lambda: True)
+        bet = Bet(2.50, Odds(2), lambda: False, lambda: True)
         self.assertTrue(bet.is_settled)
 
     def test_is_settled_raises_error_if_end_condition_is_true_and_win_condition_is_indeterminate(
         self,
     ):
-        bet = Bet(lambda: None, lambda: True)
+        bet = Bet(2.50, Odds(2), lambda: None, lambda: True)
         self.assertTrue(bet.is_settled)
 
     def test_is_settled_returns_false_if_end_condition_is_false(
         self,
     ):
-        bet = Bet(lambda: True, lambda: False)
+        bet = Bet(2.50, Odds(2), lambda: True, lambda: False)
         self.assertFalse(bet.is_settled)
 
     def test_is_settled_returns_false_if_end_condition_is_undetermined(
         self,
     ):
-        bet = Bet(lambda: True, lambda: None)
+        bet = Bet(2.50, Odds(2), lambda: True, lambda: None)
         self.assertFalse(bet.is_settled)
