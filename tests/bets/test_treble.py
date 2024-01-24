0962ca676fcebd325463e353da685d104e88ab18
peaky76 <robertjamespeacock@gmail.com>
2024-01-24 18:03:51 +0000

    refactor: make kwarg explicit in Accumulator.__new__

--- tests/bets/test_treble.py
+++ tests/bets/test_treble.py
@@ -3,6 +3,7 @@ from unittest import TestCase
 from pybet import Odds
 from pybet.bets import Treble
 
+
 class TestTreble(TestCase):
     def test_treble_raises_error_if_not_correct_number_of_selections(self):
         with self.assertRaises(ValueError):
