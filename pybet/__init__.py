8033084548fce6bc846e603f03cc7179be95a516
peaky76 <robertjamespeacock@gmail.com>
2024-01-24 17:46:05 +0000

    refactor: move bet to bets package

--- pybet/__init__.py
+++ pybet/__init__.py
@@ -1,12 +1,5 @@
-from peak_utility.number import Numbertext
-
-from .bet import Accumulator, Bet, Double, Treble
 from .market import Market
 from .odds import Odds
 
-__all__ = ["Accumulator", "Bet", "Double", "Market", "Odds", "Treble"]
+__all__ = ["Market", "Odds"]
 
-for i in range(4, 21):
-    name = f"{Numbertext(i).__str__().title()}Fold"
-    globals()[name] = type(name, (Accumulator,), {"_selection_count_requirement": i})
-    __all__ += name
