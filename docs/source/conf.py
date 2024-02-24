73882d0089aaf4caf1be4a9f9e3f233bb5d5b26e
peaky76 <robertjamespeacock@gmail.com>
2024-02-24 20:20:36 +0000

    build: update docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,8 +1,3 @@
-import os
-import sys
-
-sys.path.insert(0, os.path.abspath("../.."))
-
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -12,9 +7,9 @@ sys.path.insert(0, os.path.abspath("../.."))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Pybet"
-copyright = "2023, Robert Peacock"
+copyright = "2024, Robert Peacock"
 author = "Robert Peacock"
-release = "0.8.0"
+release = "0.8.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
